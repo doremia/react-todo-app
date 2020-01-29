8dd933a32c19cd26e05e8b35fa74ed503ac5b075
Mia Chen <techolic77@gmail.com>
2020-01-29 12:14:55 -0800

    added styling

---
+++ src/TodoData.js
@@ -0,0 +1,34 @@
+const TodoData = [
+    {
+        id:1,
+        text:"Take out the trash",
+        completed:false
+    },
+    {
+        id:2,
+        text:"Watch anime",
+        completed:true
+    },
+    {
+        id:3,
+        text:"Do laundry",
+        completed:false
+    },
+    {
+        id:4,
+        text:"Worship BTS",
+        completed:true  
+    },
+    {
+        id:5,
+        text:"Say I'm beautiful.",
+        completed:true
+    },
+    {
+        id:6,
+        text:"Massage my fat tissue",
+        completed:false
+    }
+]
+
+export default TodoData
